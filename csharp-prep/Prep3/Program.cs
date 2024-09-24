a9eafa4e4275655884ca6a4e2e1713a590ab85a9
Anderson-Damen <Andersondamen@gmail.com>
2024-09-24 12:26:19 -0600

    Sandbox group simple calculator and Prep #3 finished

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 100);
+        int guess = -1;
+
+        Console.Write("Guess the magic number between 1 and 100: ");
+
+        while (guess != number)
+        {
+            guess = int.Parse(Console.ReadLine());
+
+            if (guess > number)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (guess < number)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guess == number)
+            {
+                Console.WriteLine("You guessed it!");
+                Console.WriteLine("Would you like to play again? (yes/no)");
+                string answer = Console.ReadLine();
+                if (answer == "yes")
+                {
+                    number = randomGenerator.Next(1, 100);
+                    guess = -1;
+                    Console.Write("Guess the magic number between 1 and 100: ");
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
     }
 }
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,16 +1,54 @@
 using System;
+using System.Formats.Asn1;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Course coursel = new Course();
-        coursel._classCode = "CSE 210";
-        coursel._classname = "programming with classes";
-        coursel._credits = 2;
-        coursel._color = "Green";
+        // Course coursel = new Course();
+        // coursel._classCode = "CSE 210";
+        // coursel._classname = "programming with classes";
+        // coursel._credits = 2;
+        // coursel._color = "Green";
 
-        coursel.Display();
+        // coursel.Display();
+        string Operation = "";
 
+            while (Operation != "5")
+            {
+            Console.Write("Enter First interger: ");
+            int First_number = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter Second interger: ");
+            int Second_number = int.Parse(Console.ReadLine());
+        
+            Console.WriteLine("1. Addistion");
+            Console.WriteLine("2. Subtraction");
+            Console.WriteLine("3. Multiplication");
+            Console.WriteLine("4. Division");
+            Console.Write("Choose which operation you want to perform: ");
+            Operation = Console.ReadLine();
+
+            if (Operation == "1")
+            {
+                Console.WriteLine(First_number + Second_number);
+            }
+            else if (Operation == "2")
+            {
+                Console.WriteLine(First_number - Second_number);
+            }
+            else if (Operation == "3")
+            {
+                Console.WriteLine(First_number * Second_number);
+            }
+            else if (Operation == "4")
+            {
+                Console.WriteLine(First_number / Second_number);
+            }
+            else if (Operation == "5")
+            {
+                Console.WriteLine("Thank you for using the simple calculator!");
+            }
+        }
     }
 }
