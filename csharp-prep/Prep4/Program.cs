6e45baee1b0a2ef0d30869547ba0254376983a15
Anderson-Damen <Andersondamen@gmail.com>
2024-09-24 14:25:25 -0600

    Prep 4 code

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,62 @@
 using System;
+using System.Diagnostics.CodeAnalysis;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        int number = -1;
+        while (number != 0)
+        {
+            Console.Write("Enter number: ");
+            number = int.Parse(Console.ReadLine());
+            if (number != 0)
+            {
+                numbers.Add(number);
+            }
+            else
+            {
+                break;
+            }
+        }
+
+        int sum = 0;
+        foreach (int num in numbers)
+        {
+            sum += num;
+        }
+        Console.WriteLine("The sum is: " + (sum));
+
+        Console.WriteLine("The average is: " + (sum / numbers.Count));
+
+        int largest = 0;
+        foreach (int num in numbers)
+        {
+            if (num > largest)
+            {
+                largest = num;
+            }
+            else
+            {
+                break;
+            }
+        }
+        Console.WriteLine("The largest number is: " + (largest));
+
+        int Small = 100000;
+        foreach (int num in numbers)
+        {
+            if (num < Small && num > 0)
+            {
+                Small = num;
+            }
+            else
+            {
+                break;
+            }
+        }
+        Console.WriteLine("The smallest positive number is: " + (Small));
     }
 }
