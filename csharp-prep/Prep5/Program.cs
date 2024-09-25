861ff70eabc7496d4f51002ac7f49d46b3fd635c
Anderson-Damen <Andersondamen@gmail.com>
2024-09-24 20:40:07 -0600

    Prep 4 start

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        static void DisplayWelcome()
+        {
+            Console.WriteLine("Welcome to the Program!");
+        }
+        
+        static void PromptUserName(string user_name)
+        {
+                Console.WriteLine("Please enter your name? ");
+                user_name = Console.ReadLine();
+        }
+        
+        static void PromptUserNumber(int user_number)
+        {
+            Console.Write("Please enter your favorite number? ");
+            user_number = int.Parse(Console.ReadLine());
+        }
+
+        static int SquareNumber(int user_number)
+        {
+            return user_number * user_number;
+        }
+
+        static void DisplayResult(int squared_number, string user_name, int user_number)
     }
 }
